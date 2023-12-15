23af67d1ef88c2fee814cb298523af171bbec816
Michael-she <mgshepstone@gmail.com>
2023-12-15 15:12:11 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,29 @@
-module.exports = (req, res) => {
-    res.status(200).send('Hello from the serverless function!');
-  };
-  
+
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON body in POST requests
+app.use(express.json());
+
+// GET request handler
+app.get('/hello', (req, res) => {
+    res.send('Hello, world!');
+});
+
+// POST request handler
+app.post('/data', (req, res) => {
+    const data = req.body;
+    // Process the data here
+    res.json({
+        message: 'Data received successfully',
+        receivedData: data
+    });
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
