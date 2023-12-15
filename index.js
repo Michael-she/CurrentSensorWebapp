82c148c3d9e8d14e24413ba12bdb14f52dbc541e
Michael-she <mgshepstone@gmail.com>
2023-12-15 17:26:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
+const path = require('path'); // Import the path module for working with file paths
 
 // Middleware to parse JSON body in POST requests
 //app.use(express.json());
