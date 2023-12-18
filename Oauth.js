83ddcb9acc52aa6194be65fe4173c7db28a139fb
Michael-she <mgshepstone@gmail.com>
2023-12-18 23:00:23 +0200

    ll

--- Oauth.js
+++ Oauth.js
@@ -10,12 +10,7 @@ function handleCredentialResponse(response) {
      gdata = parseJwt(response.credential);
 
     console.log('User signed in:');
-   /* console.log('ID: ' + gdata.sub);
-    console.log('Full Name: ' + gdata.name);
-    console.log('Given Name: ' + gdata.given_name);
-    console.log('Family Name: ' + gdata.family_name);
-    console.log('Image URL: ' + gdata.picture);
-    console.log('Email: ' + gdata.email);*/
+   
 
     setID(gdata.sub);
     console.log(gdata.name)
