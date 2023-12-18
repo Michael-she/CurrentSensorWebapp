9ba8d21a9d031e6769ebb4f8d7bcae415a51e617
Michael-she <mgshepstone@gmail.com>
2023-12-19 00:37:47 +0200

    Passport

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // Set up middleware to pars
 app.use(bodyParser.json());
 
 
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 
 const csvParser = require('csv-parser');
